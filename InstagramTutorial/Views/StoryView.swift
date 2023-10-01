26ede21c105dc138723ea4a61e01911cc220cca4
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-01 20:59:38 -0300

    Creación de ViewStories Stroke con LInearGradient TagLIVE Listado horizontal stories

---
+++ InstagramTutorial/Views/StoryView.swift
@@ -0,0 +1,18 @@
+//
+//  StoryView.swift
+//  InstagramTutorial
+//
+//  Created by Christian Manzaraz on 01/10/2023.
+//
+
+import SwiftUI
+
+struct StoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryView()
+}
