d053f69c5e7b6f50e864a9533e4325474f85f3bb
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-09-30 23:37:38 -0300

    Initial Commit

---
+++ InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Christian Manzaraz on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
