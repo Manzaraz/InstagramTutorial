bbee7f08c15bb6b82f8ca7ab771141563e54b108
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-02 15:14:18 -0300

    TimelineView is working

---
+++ InstagramTutorial/Views/TimelineView.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineView.swift
+//  InstagramTutorial
+//
+//  Created by Christian Manzaraz on 01/10/2023.
+//
+
+import SwiftUI
+
+struct TimelineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimelineView()
+}
