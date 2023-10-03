987719768cb6396fa30ffcf9d88b570d6da1d98b
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-03 20:40:25 -0300

    finshing the project

---
+++ InstagramTutorial/Views/TabViewCustom.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewCustom.swift
+//  InstagramTutorial
+//
+//  Created by Christian Manzaraz on 03/10/2023.
+//
+
+import SwiftUI
+
+struct TabViewCustom: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabViewCustom()
+}
